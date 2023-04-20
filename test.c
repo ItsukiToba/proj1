c8e3251199d530e34227701681eb3fb902087ff9
cy21281 <cy21281@shibaura-it.ac.jp>
2023-04-20 14:28:00 +0900

    commit massage's test

--- test.c
+++ test.c
@@ -1,4 +1,25 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
-
 int main() {
+	char buff[10];
+	int fd;
+	int val;
+	fd = open("test.txt",O_RDONLY);
+	if (fd < 0) {
+		perror("comment:");
+		exit(1);
+	}
+	val = read(fd,buff,10);
+	if (val < 0) {
+		perror("comment:");
+		exit(1);
+	}
+	val = write(1,buff,10);
+	if (val < 0) {
+		perror("comment:");
+		exit(1);
+	}
+	close(fd);
 }
